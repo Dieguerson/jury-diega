7c7ba69d123f44297842454e8ec2f5eaa1883e27
Dieguerson <voices113@hotmail.com>
2022-07-15 14:32:02 -0300

    Jury3 target_blank

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -15,7 +15,7 @@ function UserCard(){
           <p className='font-thin mb-2 text-sm self-center italic'>{userData.name}</p>
           <p className='font-semibold text-base'>Public: {userData.public}</p>
           <p className='font-semibold text-base mb-2'>Owned Private: {userData.private}</p>
-          <a href={userData.url} className="bg-[#333] rounded-md flex text-[#f5f5f5] w-fit items-center justify-center px-2 py-1 font-bold self-center mb-2"><Icon icon="ion:logo-github" className='mr-2 w-5 h-5' /> GitHub</a>
+          <a href={userData.url} target="_blank" className="bg-[#333] rounded-md flex text-[#f5f5f5] w-fit items-center justify-center px-2 py-1 font-bold self-center mb-2"><Icon icon="ion:logo-github" className='mr-2 w-5 h-5' /> GitHub</a>
           <Link to='/projects' className="bg-[#4a50bc] rounded-md flex text-[#f5f5f5] w-fit items-center justify-center px-2 py-1 font-bold self-center">Projects</Link>
         </section>
       </article>
